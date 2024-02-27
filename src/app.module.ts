b4a4a2aff2f5b6db06b9428e4743e10b6f772a78
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2024-02-27 18:41:45 +0200

    rename imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Customer } from './model/customer.entity';
-import { Invoice } from './model/invoice.entity';
-import { Project } from './model/project.entity';
+import { Customer } from './entities/customer.entity';
+import { Invoice } from './entities/invoice.entity';
+import { Project } from './entities/project.entity';
 
 @Module({
   imports: [
