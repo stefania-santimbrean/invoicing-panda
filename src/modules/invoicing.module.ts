f39bdc66bb3e6c814ec2714d6c6fc4fcaa9603b1
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2024-02-27 19:52:59 +0200

    added jointable

---
+++ src/modules/invoicing.module.ts
@@ -0,0 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class InvoicingModule {}
