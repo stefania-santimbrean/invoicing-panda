a9f9aa2e8da4d0c870ae1bf0b5fd7d80064e8135
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2024-02-28 10:00:14 +0200

    change db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Customer } from './entities/customer.entity';
-import { Invoice } from './entities/invoice.entity';
-import { Project } from './entities/project.entity';
 import { InvoicingModule } from './modules/invoicing.module';
+import { AppConfig, DbConfig } from './config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Customer, Invoice, Project],
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Customer]),
     InvoicingModule,
---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [`${__dirname}/../../db/migrations/*{.ts, .js}`],
+  migrationTableName: 'migrations',
+}));
