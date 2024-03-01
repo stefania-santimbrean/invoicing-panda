2e36083c4f755beb2833cf3b2dee139e0000a729
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2024-03-01 13:54:29 +0200

    added tests for grapql queries

--- src/modules/apis/invoice/invoice.service.ts
+++ src/modules/apis/invoice/invoice.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Invoice } from '../../../entities/invoice.entity';
-
+import { Repository } from 'typeorm';
+import { INVOICES_DATA } from '../../../../db/mock-data/seed-data';
+export const invoice_not_found_error_message = (nr) =>
+  `No invoice was found with number ${nr}`; // ideally this would be coming from an i18n file
 @Injectable()
 export class InvoiceService {
   constructor(
     @InjectRepository(Invoice)
-    private readonly invoiceRepository,
+    private readonly invoiceRepository: Repository<Invoice>,
   ) {}
 
   async getInvoices() {
@@ -19,9 +22,18 @@ export class InvoiceService {
       .where('invoice.nr = :nr', { nr: nr })
       .getOne();
     if (!value) {
-      throw new Error(`No invoice was found with number ${nr}`); //add some i18n later aligator
+      throw new Error(invoice_not_found_error_message(nr));
       // check how to improve graphql error response and code
     }
     return value;
+  }
+
+  async seed() {
+    await this.invoiceRepository
+      .createQueryBuilder()
+      .insert()
+      .into(Invoice)
+      .values(INVOICES_DATA)
+      .execute();
   }
 }
--- src/modules/apis/invoice/invoice.spec.ts
+++ src/modules/apis/invoice/invoice.spec.ts
@@ -4,6 +4,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../../app.module';
+import { invoice_not_found_error_message } from './invoice.service';
+import { INVOICES_DATA } from '../../../../db/mock-data/seed-data';
 
 describe('Invoice Integration Tests', () => {
   let app: INestApplication;
@@ -23,13 +25,14 @@ describe('Invoice Integration Tests', () => {
     await app.close();
   });
 
-  it('should return the expected result for a GraphQL query', async () => {
+  it('should retrieve and list all invoices in the system', async () => {
     const query = `
       query {
         invoices {
-            nr
-            currency
-            amount
+          isStorno
+          currency
+          amount
+          date
       }
     }
     `;
@@ -39,24 +42,73 @@ describe('Invoice Integration Tests', () => {
       .send({ query })
       .expect(200);
 
-    // Add assertions based on the expected result
+    const invoices = INVOICES_DATA.map((inv) => ({
+      ...inv,
+      date: inv.date.getTime(),
+    }));
+
     expect(response.body).toEqual({
       data: {
-        invoices: [
-          {
-            nr: 1337,
-            currency: 'RON',
-            amount: 100,
-          },
-          {
-            nr: 1338,
-            currency: 'RON',
-            amount: 200,
-          },
-        ],
+        invoices,
       },
     });
   });
 
-  // Add more tests for mutations, subscriptions, etc.
+  it('should have the first invoice nr eq to 1337', async () => {
+    const query = `
+      query {
+        invoices {
+          nr
+      }
+    }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query })
+      .expect(200);
+
+    const minInvoiceNr = Math.min(
+      ...response.body.data.invoices.map((invoice) => invoice.nr),
+    );
+    expect(minInvoiceNr).toEqual(1337);
+  });
+
+  it('look up an invoice via its number', async () => {
+    const nr = 1337;
+    const query = `
+      query {
+        invoice(nr: ${nr}) {
+          nr
+      }
+    }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query })
+      .expect(200);
+
+    expect(response.body.data.invoice.nr).toEqual(1337);
+  });
+
+  it('look up an invoice via its number and not find it', async () => {
+    const nr = 1336;
+    const query = `
+      query {
+        invoice(nr: ${nr}) {
+          nr
+      }
+    }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query })
+      .expect(200);
+
+    expect(response.body.errors[0].message).toEqual(
+      invoice_not_found_error_message(nr),
+    );
+  });
 });
